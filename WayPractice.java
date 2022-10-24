097836e827166d45eae68add3683249d470d7b7b
11701 <logan.peek@vtsd.com>
2022-10-24 07:33:16 -0400

    110 Version

---
+++ WayPractice.java
@@ -0,0 +1,15 @@
+public class WayPractice {
+    public static void main(String args[]){
+
+
+        for(int n= 0; n <= 100; n+=3){
+
+            int k = 100 - n;
+
+            System.out.print(k + " ");
+
+        }
+
+
+    }
+}
